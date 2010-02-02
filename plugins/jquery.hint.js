5020fb458ae38f2ada17b43e5c26e2856a37a73d
Vidar Svansson <vidarsvans@gmail.com>
2010-02-02 22:56:13 +0000

    updated from an old local version

--- plugins/jquery.hint.js
+++ plugins/jquery.hint.js
@@ -1,33 +1,69 @@
-/**
- * jQuery hint plugin - Vidar Svansson (vidarsvans@gmail.com) 
- * Based on inputHint by Jason Frame (jason@onehackoranother.com)
- * Released under The MIT License.
- * usage: $(':text').hint('+kbd');
+/*
+ * (c) 2008-9 Jason Frame
+ * Auxiliary element code based on work by pjesi (http://wtf.hax.is/)
  */
-
 (function ($) {
- $.fn.hint = function(expr, options) {
-  options = $.extend({hintClass: 'hint'}, options || {});
 
- var hint = $(this).find(expr).hide().text();
+    /**
+     * Initialise input hints on all matched inputs.
+     *
+     * Usage examples:
+     *
+     * Add hints to all inputs with the 'title' attribute set:
+     *   $('input[title],textarea[title]').inputHint();
+     *
+     * Add hints to all matched elements, grabbing the hint text from each element's
+     * adjacent <kbd/> tag:
+     *   $('input').inputHint({using: '+ kbd'});
+     *
+     * Options keys:
+     *  using: jQuery selector locating element containing hint text, relative to
+     *         the input currently being considered.
+     *  hintAttr - tag attribute containing hint text. Default: 'title'
+     *  hintClass - CSS class to apply to inputs with active hints. Default: 'hint'
+     */
+    $.fn.inputHint = function(options) {
 
-  function showHint() {
-   if ($(this).val() == '') {
-                $(this).addClass(options.hintClass).val(hint);
-   }
-  }
+        options = $.extend({hintClass: 'hint', hintAttr: 'title'}, options || {});
 
-  function removeHint() {
-   if ($(this).hasClass(options.hintClass)) $(this).removeClass(options.hintClass).val('');
-  }
-  
-  this.focus(removeHint).blur(showHint).blur();
+        function hintFor(element) {
+            var h;
+            if (options.using && (h = $(options.using, element)).length > 0) {
+                return h.text();
+            } else {
+                return $(element).attr(options.hintAttr) || '';
+            }
+        }
+
+        function showHint() {
+            if ($(this).val() == '') {
+                $(this).addClass(options.hintClass).val(hintFor(this));
+            }
+        }
+
+        function removeHint() {
+            if ($(this).hasClass(options.hintClass)) {
+                $(this).removeClass(options.hintClass).val('')
+                if (this.createTextRange) {
+                    this.createTextRange().select();
+                }
+            };
+        }
+
+        this.filter(function() {
+            return !!hintFor(this);
+        }).focus(removeHint).blur(showHint).blur();
 
         var $form = this.parents('form:eq(0)');
         this.each(function() {
             var self = this;
-            $form.submit(function() { removeHint.apply(self); });
+            $form.submit(function() {
+                removeHint.apply(self);
+            });
         });
- };
- 
+
+        return this.end(); // undo filter
+
+    };
+
 })(jQuery);
